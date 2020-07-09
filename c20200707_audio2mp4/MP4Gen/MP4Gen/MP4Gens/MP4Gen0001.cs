963262147bc1d14b9291b02ad953d41faf0e7921
stackprobe <stackprobes@gmail.com>
2020-07-09 22:06:50 +0900

    Gelato

---
+++ c20200707_audio2mp4/MP4Gen/MP4Gen/MP4Gens/MP4Gen0001.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Tools;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace Charlotte.MP4Gens
+{
+	public class MP4Gen0001
+	{
+		private void Main_Go(string audioDir, string videoDir, string wDir)
+		{
+			if (Directory.Exists(audioDir) == false)
+				throw null;
+
+			if (Directory.Exists(videoDir) == false)
+				throw null;
+
+			string midVideoDir = Path.Combine(wDir, "video");
+
+			FileTools.Delete(wDir);
+			FileTools.CreateDir(wDir);
+			FileTools.CreateDir(midVideoDir);
+
+			string audioWavFile = Path.Combine(audioDir, "Wave.wav");
+
+			if (File.Exists(audioWavFile) == false)
+				throw null;
+
+			int frame;
+
+			Console.WriteLine("*1"); // test
+
+			for (frame = 0; ; frame++)
+			{
+				string rFile = Path.Combine(videoDir, string.Format("{0}.bmp", frame));
+				string wFile = Path.Combine(midVideoDir, string.Format("{0}.jpg", frame));
+
+				if (File.Exists(rFile) == false)
+					break;
+
+				if (frame % 100 == 0)
+					Console.WriteLine("frame: " + frame); // test
+
+				new Canvas2(rFile).Save(wFile, ImageFormat.Jpeg, 90);
+			}
+			if (frame < 1)
+				throw null;
+
+			Console.WriteLine("*2"); // test
+
+			ProcessTools.Batch(new string[]
+			{
+				Consts.FFMPEG_FILE + " -r 20 -i %%d.jpg ..\\video.mp4",
+			},
+			midVideoDir
+			);
+
+			if (File.Exists(Path.Combine(wDir, "video.mp4")) == false)
+				throw null;
+
+			Console.WriteLine("*3"); // test
+
+			ProcessTools.Batch(new string[]
+			{
+				Consts.FFMPEG_FILE + " -i video.mp4 -i " + audioWavFile + " -map 0:0 -map 1:0 -vcodec copy movie.mp4",
+			},
+			wDir
+			);
+
+			if (File.Exists(Path.Combine(wDir, "movie.mp4")) == false)
+				throw null;
+
+			Console.WriteLine("*4"); // test
+		}
+
+		public void Main01()
+		{
+			Main_Go(
+				@"C:\temp\a0001",
+				@"C:\temp\a1001",
+				@"C:\temp\a2001"
+				);
+		}
+	}
+}
--- c20200707_audio2mp4/MovieGen/MovieGen/MovieGen/MovieGens/MovieGen0001.cs
+++ c20200707_audio2mp4/MovieGen/MovieGen/MovieGen/MovieGens/MovieGen0001.cs
@@ -4,50 +4,65 @@ using System.Linq;
 using System.Text;
 using Charlotte.Common;
 using Charlotte.MovieGens.MGEngines;
+using Charlotte.Tools;
+using DxLibDLL;
+using System.IO;
 
 namespace Charlotte.MovieGens
 {
 	public class MovieGen0001
 	{
+		private const string R_DIR = @"C:\temp\a0001";
+		private const string W_DIR = @"C:\temp\a1001";
+
 		private SpectrumData SpData;
 		private int Frame = 0;
 
 		private void MG_EachFrame()
 		{
+#if true
+			DX.SaveDrawScreen(0, 0, DDConsts.Screen_W, DDConsts.Screen_H, Path.Combine(W_DIR, string.Format("{0}.bmp", this.Frame)));
+
+			DDEngine.EachFrame();
+#else
 			// 60hz -> 20hz
 
 			DDEngine.EachFrame(); // 1
 			DDEngine.EachFrame(); // 2
 			DDEngine.EachFrame(); // 3
+#endif
 
 			this.Frame++;
 		}
 
 		public void Main01()
 		{
-			this.SpData = new SpectrumData(@"C:\temp\a0001\Spectrum.csv");
+			FileTools.Delete(W_DIR);
+			FileTools.CreateDir(W_DIR);
 
-			foreach (DDScene scene in DDSceneUtils.Create(20))
-			{
-				// TODO
+			this.SpData = new SpectrumData(Path.Combine(R_DIR, "Spectrum.csv"));
 
-				this.MG_EachFrame();
-			}
-			foreach (DDScene scene in DDSceneUtils.Create(20))
-			{
-				// TODO
-
-				this.MG_EachFrame();
-			}
-			foreach (DDScene scene in DDSceneUtils.Create(20))
-			{
-				// TODO
-
-				this.MG_EachFrame();
-			}
 			while (this.Frame < this.SpData.Rows.Length)
 			{
-				// TODO
+				double[] row = this.SpData.Rows[this.Frame];
+
+				DDCurtain.DrawCurtain();
+				DDPrint.SetPrint(0, 20, 23);
+
+				for (int index = 0; index < 45; index++)
+				{
+					double lv = 0.0;
+
+					for (int c = 0; c < 2; c++)
+						lv += row[index * 2 + c];
+
+					int iLv = DoubleTools.ToInt(lv * 145.0);
+
+					for (int c = 0; c < iLv; c++)
+						DDPrint.Print("*");
+
+					DDPrint.PrintRet();
+				}
 
 				this.MG_EachFrame();
 			}
