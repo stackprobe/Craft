54a1195148951d9cc9502b078a1229c3bf5f1fe6
stackprobe <stackprobes@gmail.com>
2020-07-23 07:15:25 +0900

    Ange

---
+++ c20200721_JacketSpConv/ConvGenVideo/ConvGenVideo/ConvGenVideo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Charlotte.Tools;
+
+namespace Charlotte
+{
+	static class Program
+	{
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			ProcMain.GUIMain(() => new MainWin(), APP_IDENT, APP_TITLE);
+		}
+
+		public const string APP_IDENT = "{4ac26037-d5f7-4001-80c1-e50fb08d10e8}";
+		public const string APP_TITLE = "ConvGenVideo";
+	}
+}
---
+++ c20200721_JacketSpConv/JacketSpConv/JacketSpConv/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Charlotte.Tools;
+
+namespace Charlotte
+{
+	static class Program
+	{
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			ProcMain.GUIMain(() => new MainWin(), APP_IDENT, APP_TITLE);
+		}
+
+		public const string APP_IDENT = "{9e160161-8987-43d7-883a-6236e1d3dbbb}";
+		public const string APP_TITLE = "JacketSpConv";
+	}
+}
